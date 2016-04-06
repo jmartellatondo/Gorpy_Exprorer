8910105262f5268d106fe74965f999199f1ae8d3
jorge carlo martell <jmartell@live.com.mx>
2016-04-06 16:11:20 -0700

    mejora de clase pprincipal

--- frame.py
+++ frame.py
@@ -1,65 +1,47 @@
-from tkinter import *
+from Tkinter import *
+from PIL import Image, ImageTk
 import os
 
 
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.parent = master
+        self.initUI()
 
-root = Tk('eded')
+    def initUI(self):
+        
+
+        self.img_folder = ImageTk.PhotoImage(Image.open('Folder-New-01-48.png'))
+        index=0
+        for f in self.get_filepaths("/home/jmartell/Documentos/git/Gorpy_Exprorer"):
+            Label(self.parent,text=f,justify=RIGHT,bg= '#ffffff').grid(row=index,column=1)
+
+            w = Label(self.parent,image=self.img_folder)
+            w.grid(row=index,column=0)
+            w.configure(image=self.img_folder,bg= '#ffffff')
+            #w.pack(side=TOP, fill=BOTH, expand=YES)
+            index=(1+index)
+
+    def get_filepaths(self,directory):
+        
+        file_paths = []  # List which will store all of the full filepaths.
+
+        # Walk the tree.
+        for root, directories, files in os.walk(directory):
+            for filename in files:
+                # Join the two strings in order to form the full filepath.
+                filepath = filename
+                file_paths.append(filepath)  # Add it to the list.
+
+        return file_paths  # Self-explanatory.
 
 
+def main():
+    root = Tk()
+    app = Application(root)
+    app.parent.geometry('300x200+100+100')
+    app.parent.configure(background = 'white')
+    app.mainloop()
 
-def get_filepaths(directory):
-    
-    file_paths = []  # List which will store all of the full filepaths.
-
-    # Walk the tree.
-    for root, directories, files in os.walk(directory):
-        for filename in files:
-            # Join the two strings in order to form the full filepath.
-            filepath = os.path.join(root, filename)
-            file_paths.append(filepath)  # Add it to the list.
-
-    return file_paths  # Self-explanatory.
-
-# Run the above function and store its results in a variable.   
-full_file_paths = get_filepaths("C:\\Users\\jmart\\Documents\\git")
-
-
-def hello():
-    ca = "hello!"
-
-menubar = Menu(root)
-# create a pulldown menu, and add it to the menu bar
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Open", command=hello)
-filemenu.add_command(label="Save", command=hello)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu)
-
-# create more pulldown menus
-editmenu = Menu(menubar, tearoff=0)
-editmenu.add_command(label="Cut", command=hello)
-editmenu.add_command(label="Copy", command=hello)
-editmenu.add_command(label="Paste", command=hello)
-menubar.add_cascade(label="Edit", menu=editmenu)
-
-helpmenu = Menu(menubar, tearoff=0)
-helpmenu.add_command(label="About", command=hello)
-menubar.add_cascade(label="Help", menu=helpmenu)
-
-# display the menu
-root.config(menu=menubar)
-
-for f in full_file_paths:
-    w = Label(root, text=f, bg="red", fg="white")
-
-    w.pack(padx=5, pady=10, side=LEFT)
-
-w = Label(root, text="red", bg="red", fg="white")
-w.pack(padx=5, pady=10, side=LEFT)
-w = Label(root, text="green", bg="green", fg="black")
-w.pack(padx=5, pady=20, side=LEFT)
-w = Label(root, text="blue", bg="blue", fg="white")
-w.pack(padx=5, pady=20, side=LEFT)
-
-mainloop()
+main()
